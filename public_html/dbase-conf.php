d3cd2c9043a22eb6266041b16e52b9d3a951f21b
Natalie Martinez <nmarti46@jhu.edu>
2020-05-12 15:14:45 -0400

    Fixed Bootstrap, procedure examples

---
+++ public_html/dbase-conf.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost = "dbase.cs.jhu.edu"; // database server name
+$dbuser = "20sp_nmarti46"; // staff dbase username
+$dbpass = "URW66wjtEV"; // staff dbase password
+$dbname = "20sp_nmarti46_db"; // name of db in staff account
+?>
---
+++ public_html/procedures/Q1.php
@@ -0,0 +1,81 @@
+ <head>
+	<title>Number of Olympic Medalists</title>
+ </head>
+ <body>
+ <?php
+
+
+function outputResultsTableHeader() {
+    echo "<tr>";
+    echo "<th> Year </th>";
+    echo "<th> Number of Medals </th>";
+    echo "</tr>";
+}
+
+include 'open.php';
+
+$base_year = $_POST['game'];
+
+// PARSE THE STRING
+
+
+// Call the stored procedure named ShowRawScores
+// "multi_query" executes given (multiple-statement) MySQL query
+// It returns true if first statement executed successfully; false otherwise.
+// Results of first statement are retrieved via $mysqli->store_result()
+// from which we can call ->fetch_row() to see successive rows
+if ($mysqli->multi_query("CALL Q1(".$base_year.", '".$cname."');")) {
+
+    // Check if a result was returned after the call
+    if ($result = $mysqli->store_result()) {
+
+        echo "<table border=\"1px solid black\">";
+        $row = $result->fetch_row();
+
+        // If the first row of result begins with 'ERROR: ', then our
+        // stored procedure produced a relation that indicates error(s)
+        if (strcmp($row[0], 'ERROR: ') == 0) {
+            echo "<tr><th> Result </th></tr>";
+            do {
+                echo "<tr><td>" ;
+                for($i = 0; $i < sizeof($row); $i++){
+                    echo $row[$i];
+                }
+                echo "</td></tr>";
+            } while ($row = $result->fetch_row());
+
+
+        // Otherwise, we received real results, so output table
+        } else {
+
+            // Output appropriate table header row
+            outputResultsTableHeader();
+	    
+            // Output each row of resulting relation
+            do {
+                echo "<tr>";
+                for($i = 0; $i < sizeof($row); $i++){
+                    echo "<td>" . $row[$i] . "</td>";
+                }
+                echo "</tr>";
+            } while($row = $result->fetch_row());
+        }
+
+        echo "</table>";
+        $result->close();
+
+    }
+
+// The "multi_query" call did not end successfully, so report the error
+// This might indicate we've called a stored procedure that does not exist,
+// or that database connection is broken
+} else {
+        printf("<br>Error: %s\n", $mysqli->error);
+}
+
+// Close the connection created above by including 'open.php' at top of this file
+mysqli_close($mysqli);
+
+
+ ?>
+ </body>
